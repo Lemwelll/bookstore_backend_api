762c8332955e680656b0c138ab5c4b3f4eceee39
Lemwelll <lbayson_220000001812@uic.edu.ph>
2024-05-02 14:44:22 +0800

    cors2

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ app = FastAPI()
 origins = [
     "http://http://localhost:8000",
     "https://uicbookstore.netlify.app",
-    
 ]
 
 
@@ -24,8 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"]
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include CRUD routes from modules
