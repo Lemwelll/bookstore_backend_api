19fbfde0db580edfb7d6e2b5a20920a7b65da702
Lemwelll <lbayson_220000001812@uic.edu.ph>
2024-05-06 11:22:10 +0800

    fixed4

--- model/db.py
+++ model/db.py
@@ -2,22 +2,22 @@
 import mysql.connector
 
 
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "bookstore_grp13",
-    "port": 3306,
-}
-
-
 # db_config = {
-#     "host": "139.59.246.2",
-#     "user": "thisisbookstore",
-#     "password": "thisisgrp13",
+#     "host": "localhost",
+#     "user": "root",
+#     "password": "",
 #     "database": "bookstore_grp13",
 #     "port": 3306,
 # }
+
+
+db_config = {
+    "host": "139.59.246.2",
+    "user": "thisisbookstore",
+    "password": "thisisgrp13",
+    "database": "bookstore_grp13",
+    "port": 3306,
+}
 
 def get_db():
     db = mysql.connector.connect(**db_config)
